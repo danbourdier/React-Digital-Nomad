67a852ad427670d0d34cee844844c1a227d04950
DanBourdier <danbourdier@aol.com>
2020-10-21 02:22:47 -0400

    enabled cross component functionality

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Player = () => {
 
+
   return (
     <div>
       <div id="player"></div>
-      
+      <button onClick={() => { window.player.loadVideoById('Pca24nzCdu0')} }>Click me</button>
     </div>
   )
 
