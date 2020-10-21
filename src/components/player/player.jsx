48459f63cf080aec208b84b37086e6b6c1f2e9e2
DanBourdier <danbourdier@aol.com>
2020-10-21 01:11:05 -0400

    fixed pesky youtube api hook

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,45 +1,12 @@
 import React from 'react'
-import '../../stylesheets/player.css'
 
 const Player = () => {
 
-  // 3. This function creates an <iframe> (and YouTube player)
-  //    after the API code downloads.
-  // let player;
-  // const onYouTubeIframeAPIReady = () => {
-  //   player = new YT.Player('player', {
-  //     height: '390',
-  //     width: '640',
-  //     videoId: 'M7lc1UVf-VE',
-  //     events: {
-  //       'onReady': onPlayerReady,
-  //       'onStateChange': onPlayerStateChange
-  //     }
-  //   });
-  // }
-
-  // // 4. The API will call this function when the video player is ready.
-  // const onPlayerReady = event => {
-  //   event.target.playVideo();
-  // }
-
-  // // 5. The API calls this function when the player's state changes.
-  // //    The function indicates that when playing a video (state=1),
-  // //    the player should play for six seconds and then stop.
-  // let done = false;
-  // const onPlayerStateChange = event => {
-  //   if (event.data == YT.PlayerState.PLAYING && !done) {
-  //     setTimeout(stopVideo, 6000);
-  //     done = true;
-  //   }
-  // }
-  // const stopVideo = () => {
-  //   player.stopVideo();
-  // }
-
-
   return (
-    <div id="player"></div>
+    <div>
+      <div id="player"></div>
+      
+    </div>
   )
 
 }
