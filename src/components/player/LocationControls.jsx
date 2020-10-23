78810f3f01419cd5e16d75468effb7051219f530
DanBourdier <danbourdier@aol.com>
2020-10-23 00:56:23 -0400

    changed button styling

--- src/components/player/LocationControls.jsx
+++ src/components/player/LocationControls.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const LocationControls = () => (
   <div className="location-controls-container">
     <section className="location-controls-flex-wrapper">
-      <button id="location-controls-button" onClick={() => { window.player.loadVideoById('JGaaU8j-9BI') }}>Previous</button>
-      <button id="location-controls-button">Next</button>
+      <button id="location-controls-button" onClick={() => { window.player.loadVideoById('JGaaU8j-9BI') }}> { '<<' } </button>
+      <button id="location-controls-button" onClick={() => { window.player.loadVideoById('JGaaU8j-9BI') }}> { '>>' } </button>
     </section>
   </div>
 )
