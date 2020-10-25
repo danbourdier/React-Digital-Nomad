9acf48bce79f711b11d59c8c1972b773b9fee7c5
DanBourdier <danbourdier@aol.com>
2020-10-24 20:32:48 -0400

    added context provider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import Player from './player/Player'
+//  music: { 'Tokyo': {'urls': [], 'idx': 0}, 'NYC': [], 'France': [], 'SanFrancisco': [], 'Instanbul': [], 'Shanghai': [], 'Delhi': [], 'Moscow': [], 'Toronto': []  }
+export const locationURLs = React.createContext({ 'location': { urls: ['rAeN7TdGq4o', 'yIMDgPKgN1w'], 'idx': 0 }, })
 
 const App = () => {
 
