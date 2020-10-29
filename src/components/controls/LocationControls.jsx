0bf6153d0a75c06422ffcf41cbc43806822c422f
DanBourdier <danbourdier@aol.com>
2020-10-28 23:24:12 -0400

    fixed location tracking issue

--- src/components/controls/LocationControls.jsx
+++ src/components/controls/LocationControls.jsx
@@ -14,15 +14,19 @@ const LocationControls = () => {
 
   // This is our method to switch to the next location video in our player
   const nextVid = () => {
-    setIndex(index++) // i need to increment our our curr idx
-    setCountry(cntxt[index % cntxt.length ][0]) // i need to track our current country
-    return cntxt[index % cntxt.length ][1]
+    let newIdx = index + 1
+    setIndex(newIdx) // i need to increment our our curr idx
+    console.log(newIdx)
+    setCountry(cntxt[newIdx % cntxt.length ][0]) // i need to track our current country
+    return cntxt[newIdx % cntxt.length ][1]
   }
 
   // This is the ooposite of above
   const prevVid = () => {
-    cntxt.index = cntxt.index == 0 ? cntxt.length - 1 : cntxt.index - 1
-    return cntxt[cntxt.index]
+    let newIdx = index == 0 ? cntxt.length - 1 : index - 1
+    setIndex( newIdx )
+    console.log(newIdx)
+    return cntxt[newIdx][1]
   }
 
   return (
