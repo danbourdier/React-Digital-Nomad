0b29a3b9a4a77aeabf228b52029b271d6542fb19
DanBourdier <danbourdier@aol.com>
2020-11-12 23:42:47 -0500

    added API method call to next video call

--- src/components/controls/MusicControls.jsx
+++ src/components/controls/MusicControls.jsx
@@ -3,7 +3,7 @@ import { musicURL } from '../player/Player'
 
 const MusicControls = props => {
   const [ index, setIndex ] = useState(0)
-  const [ track, setTrack ] = useState('')
+  const [track, setTrack] = useState('<< Click to navigate tracks! >>')
   const loadNext = arg => props.loadFunc(arg, 'musicPlayer')
   
   // everytime theres change (state or props), i want to load a vid with a respective key
@@ -11,26 +11,28 @@ const MusicControls = props => {
     // a non-*cleanup* effect alone would cause my application to break because the YT player
       // is not constructed in initial render. Therefore using its API calls would throw a TypeError
     return () => { 
-      loadNext(nextVid())
+      loadNext(nextVid());
     } // under the hood, #useEffect makes use of currying to call this function in a #componentDidUnmount fashion
     // this might lead to performance issues on a bigger application due to causing a 
       // re-render everytime we skip a track. (our state changes every call to #nextVid)
-  })
+  }, [props, index])
  
   const preCntxt = useContext(musicURL) // our context we made use of
   let cntxt = preCntxt[props.currCountr]  // with a separation of concerns
 
   // This is our method to switch to the next location video in our player
-  const nextVid = () => {
+  const nextVid = () => { 
     let newIdx = index + 1 // because #setState is async, we have to create a new var. To keep our function sync
     setIndex(newIdx) // i need to increment our our curr idx
+    getVideoData(cntxt[newIdx % cntxt.length])
     return cntxt[newIdx % cntxt.length] // we return the videoId located at the Ith idx at [1]
-  }
+  } 
 
   // This is the ooposite of above
   const prevVid = () => {
     let newIdx = index == 0 ? cntxt.length - 1 : index - 1
     setIndex(newIdx)
+    getVideoData(cntxt[newIdx])
     return cntxt[newIdx] 
   }
 
@@ -42,13 +44,14 @@ const MusicControls = props => {
         'snippet' // The returned JSON gives us a key with info containing title
       ],
       'id': 
-        `${vidId[index]}` // To fetch read-only data we need to pass in vidIds
+        `${vidId}` // To fetch read-only data we need to pass in vidIds
     }).then(res => {
       // return console.log('Success, the response is:', res?.result?.items[0]?.snippet?.title )
       setTrack(res?.result?.items[0]?.snippet?.title)
-      return res?.result?.items[0]?.snippet?.title
+      return res.result.items[0].snippet.title
     }) 
-      .catch(err => alert('Please allow a couple seconds before switching songs'))
+      // .catch(err => alert('Please allow a couple seconds before switching songs'))
+      .catch(err => console.log(err))
 
   )
 
@@ -56,12 +59,12 @@ const MusicControls = props => {
     <div className="Music-controls-container">
       <section className="Music-controls-flex-wrapper">
 
-        <button id="Music-controls-button" onClick={() => { getVideoData(cntxt) } }> 
-          <span>{'<<'}</span>
-        </button>
-        {/* <button id="Music-controls-button" onClick={() => { loadNext(prevVid()) }}> 
+        {/* <button id="Music-controls-button" onClick={() => { getVideoData(cntxt) } }> 
           <span>{'<<'}</span>
         </button> */}
+        <button id="Music-controls-button" onClick={() => { loadNext(prevVid()) }}> 
+          <span>{'<<'}</span>
+        </button>
 
         <figure id="track-name-container">
           <span>{ track }</span>
