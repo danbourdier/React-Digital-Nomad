ec4e2815a298340666b0b9ca0fd53775ab0ba9fa
DanBourdier <danbourdier@aol.com>
2020-12-02 20:18:34 -0500

    restructured music Controls component to re-render to changes in it's provider and not useEffect logic

--- src/components/controls/MusicControls.jsx
+++ src/components/controls/MusicControls.jsx
@@ -4,19 +4,27 @@ import { musicURL } from '../player/Player'
 const MusicControls = props => {
   const [ index, setIndex ] = useState(0)
   const [ track, setTrack]  = useState('<< Click to navigate tracks! >>')
+
   const loadNext = arg => props.loadFunc(arg, 'musicPlayer')
   
-  useEffect(() => { 
-    // a non-*cleanup* effect alone would cause my application to break because the YT player
-      // is not constructed in initial render. Therefore using its API calls would throw a TypeError
-    return () => { 
-      loadNext(nextVid());
-    }
-  }, [props, index])
+  // useEffect(() => { 
+  //   // a non-*cleanup* effect alone would cause my application to break because the YT player
+  //     // is not constructed in initial render. Therefore using its API calls would throw a TypeError
+  //   return () => { 
+  //     loadNext(nextVid());
+  //   }
+  // // }, [props])
+  // }, [ index ])
+
+  console.log('music Controls re-rendered')
 
 
-  const preCntxt = useContext( musicURL ) // our context we made use of
-  let cntxt = preCntxt[ props.currCountr ]  // with a separation of concerns
+// I am accessing array with track ids in these below
+  const cntxt = useContext( musicURL ) // our context we made use of
+  const tracks = props.trackLists[ cntxt ]  // with a separation of concerns
+
+  // const preCntxt = useContext( musicURL ) // our context we made use of
+  // let cntxt = preCntxt[ props.currCountr ]  // with a separation of concerns
 
 
   // This is our method to switch to the next location video in our player
@@ -24,18 +32,20 @@ const MusicControls = props => {
     let newIdx = index + 1 // because #setState is async, we have to create a new var. To keep our function sync
 
     setIndex( newIdx ) // i need to increment our our curr idx
-    getVideoData( cntxt[newIdx % cntxt.length] )
-    return cntxt[ newIdx % cntxt.length]  // we return the videoId located at the Ith idx at [1]
+    getVideoData( tracks[newIdx % tracks.length] )
+    return tracks[ newIdx % tracks.length]  // we return the videoId located at the Ith idx at [1]
+    // return tracks[ newIdx % tracks.length]  // we return the videoId located at the Ith idx at [1]
   } 
 
 
   // This is the ooposite of above
   const prevVid = () => {
-    let newIdx = index == 0 ? cntxt.length - 1 : index - 1
+    let newIdx = index == 0 ? tracks.length - 1 : index - 1
+
 
     setIndex( newIdx )
-    getVideoData( cntxt[newIdx] )
-    return cntxt[ newIdx ] 
+    getVideoData( tracks[newIdx] )
+    return tracks[ newIdx ] 
   }
 
 
--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -3,6 +3,16 @@ import React, { useState } from 'react'
 import LocationControls from '../controls/LocationControls'
 import MusicControls from '../controls/MusicControls'
 
+const tracks = {
+  'HongKong': ['lA0pgGNvmv4', 'gdZLi9oWNZg', 'NGcXJ-ypK3I', 'elRS0ntrEjk', 'gLXNJou6-DQ'],
+  'NYC': ['iY5j1fM-9DU', 'lE9KoKgekh4', 'C9yNSHtlg7g', 'QByNsl0SUAI',],
+  'Columbia': ['PyQDyI21x6U', 'lHUDw2hSua8', '1xNstpObA7s', 'qKp1f7Vn9dM'],
+  'France': ['lEfkziQSmZI', 'Lguwm-zcbTs', 'IFcxEfRO3n4', 'D9znOPZX5MQ'],
+  'Germany': ['M9zSFf63hDM', 'FR5co0KKmlE', 'iwGKiDsfXlU', 'UCbmg6yDqww'],
+  'India': ['Q48tagwurUw', '_S__Nu_5KSU', 'hJBHSmyqv0Y', 'UCbmg6yDqww'],
+  'Italy': ['wwsIGR11YmY', '8wlxTdDol70', 'U7hY7VVm7FQ', 'sNYuwmQCn-s'],
+}
+
 export const locationURL = React.createContext(null)
 export const musicURL = React.createContext(null)
 
@@ -17,15 +27,10 @@ const Player = () => {
 
   return (
     <div id="location-player-container">
-      <musicURL.Provider value={{ 'HongKong': ['lA0pgGNvmv4', 'gdZLi9oWNZg', 'NGcXJ-ypK3I', 'elRS0ntrEjk', 'gLXNJou6-DQ'], 
-                                  'NYC': ['iY5j1fM-9DU', 'lE9KoKgekh4', 'C9yNSHtlg7g', 'QByNsl0SUAI',], 
-                                  'Columbia': ['PyQDyI21x6U', 'lHUDw2hSua8', '1xNstpObA7s', 'qKp1f7Vn9dM'], 
-                                  'France': ['lEfkziQSmZI', 'Lguwm-zcbTs', 'IFcxEfRO3n4', 'D9znOPZX5MQ'], 
-                                  'Germany': [ 'M9zSFf63hDM', 'FR5co0KKmlE', 'iwGKiDsfXlU', 'UCbmg6yDqww'], 
-                                  'India': [ 'Q48tagwurUw', '_S__Nu_5KSU', 'hJBHSmyqv0Y', 'UCbmg6yDqww'], 
-                                  'Italy': [ 'wwsIGR11YmY', '8wlxTdDol70', 'U7hY7VVm7FQ', 'sNYuwmQCn-s'], 
-                                  } }>
-        <MusicControls currCountr={ currentCountry } loadFunc={ loadNext } />
+      <musicURL.Provider value={ currentCountry }>
+
+        <MusicControls trackLists={ tracks } loadFunc={ loadNext } />
+
       </musicURL.Provider>
 
       <div id="unclickable-overlay"></div>
@@ -35,7 +40,9 @@ const Player = () => {
                                     'Columbia': 'FBDLlpg1P_I', 'Germany': 'mlS89Cd176M', 
                                     'HongKong': 'H50az3Aq7x4', 'India': 'XRU9omrRV64', 
                                     'Italy': 'lpo62RjldVA' } }>
+
         <LocationControls updateCurr={setCountry} loadFunc={ loadNext } />
+        
       </locationURL.Provider>
     </div>
   )
