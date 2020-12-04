96e36d4e305c4d74d362be09407ef7156ecdaf11
DanBourdier <danbourdier@aol.com>
2020-12-03 21:47:55 -0500

    successfully abstracted DataAPI code

--- src/utils/YTDataAPI.js
+++ src/utils/YTDataAPI.js
@@ -1,3 +1,10 @@
+// 1* Before we access access any API belonging to Google, we must set our key, and load the discovery path with the client library we loaded
+// 2* Here we are loading the discovery URL that allows access to the specific location we can call methods on
+    // Its our way of saying this is what we want access to (within google's client service) so we can request youtube vid data!
+// 3* When it loads, our client constructor then appends the methods generated from the URL
+// 4* Before using any google APIs we need to enable access to our middle man that routes us access to a specific library (Its like we are asking and threading to what we want )
+  // This method allows a second argument (callback) to be executed upon our promise resolving to our target library
+    // For reference, https://github.com/google/google-api-javascript-client/blob/master/docs/reference.md
 
 const loadClient = () => { // 1*
   gapi.client.setApiKey('AIzaSyDp-Ef6SpV-umrNe9-8TvnmPWiuFylJbNw')// 2*
@@ -7,11 +14,4 @@ const loadClient = () => { // 1*
 }
 
 gapi.load('client', loadClient) // 4* 
-    // 1* Before we access access any API belonging to Google, we must set our key, and load the discovery path with the client library we loaded
-    // 2* Here we are loading the discovery URL that allows access to the specific location we can call methods on
-        // Its our way of saying this is what we want access to (within google's client service) so we can request youtube vid data!
-    // 3* When it loads, our client constructor then appends the methods generated from the URL
-    // 4* Before using any google APIs we need to enable access to our middle man that routes us access to a specific library (Its like we are asking and threading to what we want )
-      // This method allows a second argument (callback) to be executed upon our promise resolving to our target library
-        // For reference, https://github.com/google/google-api-javascript-client/blob/master/docs/reference.md
 console.log('YT Data API successfully loaded')
