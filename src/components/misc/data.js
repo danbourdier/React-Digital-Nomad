1b66bf0a6bc6db9ca5df91b6f498ce4f88560777
DanBourdier <danbourdier@aol.com>
2020-12-03 23:18:47 -0500

    abstracted passable API data

---
+++ src/components/misc/data.js
@@ -0,0 +1,17 @@
+
+export const tracks = {
+  'HongKong': ['lA0pgGNvmv4', 'gdZLi9oWNZg', 'NGcXJ-ypK3I', 'elRS0ntrEjk', 'gLXNJou6-DQ'],
+  'NYC': ['iY5j1fM-9DU', 'lE9KoKgekh4', 'C9yNSHtlg7g', 'QByNsl0SUAI',],
+  'Columbia': ['PyQDyI21x6U', 'lHUDw2hSua8', '1xNstpObA7s', 'qKp1f7Vn9dM'],
+  'France': ['lEfkziQSmZI', 'Lguwm-zcbTs', 'IFcxEfRO3n4', 'D9znOPZX5MQ'],
+  'Germany': ['M9zSFf63hDM', 'FR5co0KKmlE', 'iwGKiDsfXlU', 'UCbmg6yDqww'],
+  'India': ['Q48tagwurUw', '_S__Nu_5KSU', 'hJBHSmyqv0Y', 'UCbmg6yDqww'],
+  'Italy': ['wwsIGR11YmY', '8wlxTdDol70', 'U7hY7VVm7FQ', 'sNYuwmQCn-s'],
+}
+
+export const locations = {
+  'France': 'u3ayPmL2KN4', 'NYC': 'eZe4Q_58UTU',
+  'Columbia': 'FBDLlpg1P_I', 'Germany': 'mlS89Cd176M',
+  'HongKong': 'H50az3Aq7x4', 'India': 'XRU9omrRV64',
+  'Italy': 'lpo62RjldVA'
+}
--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -3,22 +3,7 @@ import React, { useState } from 'react'
 import LocationControls from '../controls/LocationControls'
 import MusicControls from '../controls/MusicControls'
 
-const tracks = {
-  'HongKong': ['lA0pgGNvmv4', 'gdZLi9oWNZg', 'NGcXJ-ypK3I', 'elRS0ntrEjk', 'gLXNJou6-DQ'],
-  'NYC': ['iY5j1fM-9DU', 'lE9KoKgekh4', 'C9yNSHtlg7g', 'QByNsl0SUAI',],
-  'Columbia': ['PyQDyI21x6U', 'lHUDw2hSua8', '1xNstpObA7s', 'qKp1f7Vn9dM'],
-  'France': ['lEfkziQSmZI', 'Lguwm-zcbTs', 'IFcxEfRO3n4', 'D9znOPZX5MQ'],
-  'Germany': ['M9zSFf63hDM', 'FR5co0KKmlE', 'iwGKiDsfXlU', 'UCbmg6yDqww'],
-  'India': ['Q48tagwurUw', '_S__Nu_5KSU', 'hJBHSmyqv0Y', 'UCbmg6yDqww'],
-  'Italy': ['wwsIGR11YmY', '8wlxTdDol70', 'U7hY7VVm7FQ', 'sNYuwmQCn-s'],
-}
-
-const locations = {
-  'France': 'u3ayPmL2KN4', 'NYC': 'eZe4Q_58UTU',
-  'Columbia': 'FBDLlpg1P_I', 'Germany': 'mlS89Cd176M',
-  'HongKong': 'H50az3Aq7x4', 'India': 'XRU9omrRV64',
-  'Italy': 'lpo62RjldVA'
-}
+import { tracks, locations } from '../misc/data.js'
 
 
 export const locationURL = React.createContext(null)
@@ -32,6 +17,7 @@ const Player = () => {
     try {
       window[playType]?.loadVideoById({ videoId: vidId }) 
     } catch( error ) {
+      console.log( `The following error has occured ${ error }. Restarting application. ` )
       window.history.go()
     }
 
@@ -52,7 +38,7 @@ const Player = () => {
         <LocationControls updateCurr={ setCountry } loadFunc={ loadNext } />
       </locationURL.Provider>
     </div>
-  )
+  ) 
 
 }
 
