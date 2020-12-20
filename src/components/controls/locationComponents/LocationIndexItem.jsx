8b9ca72a7d6fe0fb7d38645d8011c100226c8f36
DanBourdier <danbourdier@aol.com>
2020-12-19 23:26:32 -0500

    refactored sub-city CSS

--- src/components/controls/locationComponents/LocationIndexItem.jsx
+++ src/components/controls/locationComponents/LocationIndexItem.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LocationIndexItem = props => {
+  const [ visible, changeVisibility ] = useState( false )
   const { label, videoId } = props
 
 
+  const handleHover = event => {
+    changeVisibility( !visible)
+  }
+  // mouseEnter. mouseLeave
+
   return (
     <article className="city-labels" >
+      
       <section className='city-label-sections'> 
-        <span> { label } </span>
+        <span onMouseEnter={ handleHover }> { label } </span>
       </section>
-      {/* <section className='city-label-sections'> { videoId } </section> */}
+
+      <section className='city-label-sections' onMouseOut={ handleHover } style={ visible ? { visibility: visible } : { visibility: "hidden" } } >
+          <ul className="sub-city-list"> 
+            <li>123</li>
+            <li>2asdfasfasdfasfdasdfasf</li>
+            <li>3sdfasdfsadfasf</li>
+          </ul>
+      </section>
+
     </article>
   )
 }
