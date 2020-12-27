e9792c78413e21aa8316e49a7b47af6ef47d5f82
DanBourdier <danbourdier@aol.com>
2020-12-27 18:09:51 -0500

    changed default france track

--- src/components/misc/data.js
+++ src/components/misc/data.js
@@ -1,11 +1,11 @@
 
 export const tracks = {
-  'Japan': ['lA0pgGNvmv4', 'gdZLi9oWNZg', 'NGcXJ-ypK3I', 'elRS0ntrEjk', 'gLXNJou6-DQ'],
-  'NYC': ['iY5j1fM-9DU', 'lE9KoKgekh4', 'C9yNSHtlg7g', 'QByNsl0SUAI',],
-  'France': ['lEfkziQSmZI', 'Lguwm-zcbTs', 'IFcxEfRO3n4', 'D9znOPZX5MQ'],
-  'Germany': ['M9zSFf63hDM', 'FR5co0KKmlE', 'iwGKiDsfXlU', 'UCbmg6yDqww'],
-  'India': ['Q48tagwurUw', '_S__Nu_5KSU', 'hJBHSmyqv0Y', 'UCbmg6yDqww'],
-  'Italy': ['wwsIGR11YmY', '8wlxTdDol70', 'U7hY7VVm7FQ', 'sNYuwmQCn-s'],
+  'Japan': [ 'lA0pgGNvmv4', 'gdZLi9oWNZg', 'NGcXJ-ypK3I', 'elRS0ntrEjk', 'gLXNJou6-DQ' ],
+  'NYC': [ 'iY5j1fM-9DU', 'lE9KoKgekh4', 'C9yNSHtlg7g', 'QByNsl0SUAI', ],
+  'France': [ 'Lguwm-zcbTs', 'lEfkziQSmZI', 'IFcxEfRO3n4', 'D9znOPZX5MQ' ],
+  'Germany': ['M9zSFf63hDM', 'FR5co0KKmlE', 'iwGKiDsfXlU', 'UCbmg6yDqww' ],
+  'India': [ 'Q48tagwurUw', '_S__Nu_5KSU', 'hJBHSmyqv0Y', 'UCbmg6yDqww' ],
+  'Italy': [ 'wwsIGR11YmY', '8wlxTdDol70', 'U7hY7VVm7FQ', 'sNYuwmQCn-s' ],
 }
 
 export const locations = {
