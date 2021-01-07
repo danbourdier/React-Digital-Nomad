00aa89ef90470da23e6bd4ab685ec88c0587acef
DanBourdier <danbourdier@aol.com>
2021-01-06 20:36:17 -0500

    changed default state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ const importMediaAPI = () => import( /* webpackChunkName: "YTMediaAPI"  */   '..
 
 
 const App = () => {
-  const [ currentCountry, setCountry ] = useState('France')
+  const [ currentCountry, setCountry ] = useState('NYC')
 
   useEffect( () => {
     importDataAPI()
--- src/components/footerComponents/FooterContainer.jsx
+++ src/components/footerComponents/FooterContainer.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import MusicContainer from '../controls/musicComponents/MusicContainer'
 
 
-
 const FooterContainer = props => {
 
-
-
+  
   return (
     <div className='footer-container'>
       <MusicContainer trackLists={ props.trackLists } loadFunc={ props.mediaLoader } />  
